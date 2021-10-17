fb119921273b2c6424f743c4b2d53200ee780a39
Philipp <philippbunke@gmail.com>
2021-10-17 19:51:49 +0200

    Initial commit

---
+++ __init_config__.py
@@ -0,0 +1,19 @@
+
+"""
+Stores all configurations and infos to initialize the database
+"""
+
+INDEX_TICKER = ["SPX Index",
+          "NDX Index",
+          "RTY Index",
+          "RAY Index",
+          "SXXP Index",
+          "DAX Index",
+          "MDAX Index",
+          "TDXP Index"
+          "CAC Index",
+          "UKX Index",
+          "FTSEMIB Index",
+          "HSI Index",
+          "TPX Index",
+          ]
---
+++ backend.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import xbbg
+import sqlite3 as sql
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello")
